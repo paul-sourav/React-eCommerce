7100b64a1b5db5b0c9aff3c95605d2fcebecf104
sourav paul <souravpaul12345.sp@gmail.com>
2023-09-02 16:20:44 +0530

    added navbar

--- ecommerce/src/components/Layout/Header.js
+++ ecommerce/src/components/Layout/Header.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import {NavLink} from "react-router-dom";
+import {HiShoppingCart} from "react-icons/hi";
 
 const Header = () => {
   return (
-    <div className='header'>
-      Header
+    <div className="header">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <NavLink to={"/"} className="navbar-brand">
+            Navbar
+          </NavLink>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink to={'/'} className="nav-link active" aria-current="page">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to={"/link"} className="nav-link">
+                  Link
+                </NavLink>
+              </li>
+            </ul>
+            <div className="nav-item">
+                <NavLink to={'/cart'} className="nav-link">
+                  <HiShoppingCart/>
+                </NavLink>
+              </div>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
